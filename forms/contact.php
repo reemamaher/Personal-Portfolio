e2b5e9360b2b846e039464f58a9181e2f2386f0e
reemamaher <al7arbireema@gmail.com>
2023-10-05 00:24:27 +0300

    edits to contact from
    made a few edits to add smtp for contact me section

--- forms/contact.php
+++ forms/contact.php
@@ -23,20 +23,19 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  //Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'smtp.gmail.com',
+    'username' => 'reema.dailyy@gmail.com',
+    'password' => 'uvwoxzflirdxdtvi',
+    'port' => '465'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
-  $contact->recaptcha_secret_key = '6LdLiXEoAAAAADgAalV4saTKmw2hJtshgePWJCwP';
 ?>
